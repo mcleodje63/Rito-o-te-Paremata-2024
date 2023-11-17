dcc57caafee003db7093ad68d57ccaa859f55c1e
mcleodje63 <mcleodje63@craighead.school.nz>
2023-11-17 20:50:48 +1300

    done for tonight

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,6 +1,9 @@
 <header>
   <h1 class="title">Rito o te Pāremata 2024</h1>
-  <h2><img src="gmail.png" alt="gmail" /><img src="linkedin.png" alt="linkedin" /><img src="instagram.png" alt="instagram" /></h2>
+  <h2>
+    <a href="mailto:mcleodje63@craighead.school.nz"><img src="gmail.png" alt="gmail" /></a><a href="https://www.linkedin.com/in/jessie-mcleod-a44b3a249"><img src="linkedin.png" alt="linkedin" /></a>
+    <a href="https://instagram.com/jessiejammcleod?igshid=MzMyNGUyNmU2YQ=="><img src="instagram.png" alt="instagram" /></a>
+  </h2>
 </header>
 
 <style>
@@ -13,7 +16,8 @@
   h1 {
     margin: 0;
     font-size: 3.7vw;
-    /* color: #20cca4; */
+    color: #20cca4;
+    text-shadow: 0.5px 0.5px 0.5px black;
   }
   img {
     width: 40px;
