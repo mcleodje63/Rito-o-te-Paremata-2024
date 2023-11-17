cbfdfcce4ec028b35f0b6199f1b6cbdb2e1042ea
mcleodje63 <mcleodje63@craighead.school.nz>
2023-11-18 11:09:32 +1300

    .

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -3,6 +3,7 @@
     Created by <a href="https://github.com/mcleodje63"><span> Jessie McLeod</span></a> 2023
     <!--  should take them to a github profile  -->
   </p>
+  <p>Icons by <a href="https://www.flaticon.com/authors/freepik">Freepik</a></p>
 </footer>
 
 <style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,10 +8,12 @@
   });
 </script>
 
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
 <main>
   <header><Header /></header>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
   <div class="cropped-image">
     <video id="myVideo" max-width="100%" height="auto" autoplay loop muted>
       <source src="rito2.0.mp4" type="video/mp4" />
@@ -20,19 +22,20 @@
       <h2>Jessie McLeod</h2>
     </div>
   </div>
-  <div class="row">
-    <section class="section content left">
-      <p>My name is Jessie McLeod, I’m 16, and a year 12 student at Craighead Dioscean I live in Timaru, a port town two hours south of Christchurch, and have for my whole life. This constant stability in a chaotic world has allowed me to get involved and make connections in a variety of areas which I hold a passion for. In my spare time I enjoy spending time outdoors with my friends and family, cooking, reading and writing poetry, but also coding, photography, watching tv, football and playing guitar.</p>
-    </section>
-    <div class="right">
+  <section class="content section columns">
+    <article class="column left">
+      <p>My name is Jessie McLeod, I’m 16, and a year 12 student at Craighead Dioscean. I live in Timaru, a port town two hours south of Christchurch, and have for my whole life. This constant stability in a chaotic world has allowed me to get involved and make connections in a variety of areas which I hold a passion for. In my spare time I enjoy spending time outdoors with my friends and family, cooking, reading and writing poetry, but also coding, photography, watching tv, football and playing guitar.</p>
+    </article>
+    <figure class="column right">
       <img src="seaweek.jpg" alt="Seaweek" />
-    </div>
-  </div>
-  <div class="row">
-    <div class="left">
+      <!-- remember to link to news article  -->
+    </figure>
+  </section>
+  <section class="content section columns">
+    <figure class="column left">
       <img src="scumna.JPG" alt="sumnerYouthGroup" />
-    </div>
-    <section class="selection content right">
+    </figure>
+    <article class="column right">
       <ul>
         <li>Top SVA Volunteer Craighead 2023</li>
         <li>Member of national Anglican General Snyod 2024</li>
@@ -40,16 +43,18 @@
       </ul>
       <p>Since the start of this year, I’ve been on Vestry at St John’s Anglican Church, and attended Christchurch Dioscean Synod, which has been a great experience for me, and I plan to remain in this role until I’ve graduated high school. Another aspect of St. Johns Church which I am heavily involved in, is our church's A Rocha’s EcoChurch group, which involves working in organising events, teaming up with people from different generations/backgrounds, and sending a lot of emails.</p>
       <br />
-    </section>
-  </div>
-  <div class="row">
-    <section class="selection content left">
+    </article>
+  </section>
+  <section class="content section columns">
+    <article class="column left">
       <p>During my time at Craighead, I was lucky enough to be selected to attend two model EU’s held at the University of Canterbury, which allowed me to explore diplomacy and working with others. Volunteering for my community has also been very important to me, I’ve been involved in helping with the Craighead uniform shop, Sunday school, Sustainable Coastlines, early vote counting, as well as many others, and gathered over 100 hours on the SVA website.</p>
       <br />
       <p>I want to be a part of Rito to discover hands on the inner workings of a system which affects me and learn more about Aotearoa New Zealand’s government. I may not know much about Parliament, however I’m naturally a curious and interested person, so this role is right for me.</p>
-    </section>
-    <div class="right"><img src="@MtCook.JPG" width="100%" alt="mountCook" /></div>
-  </div>
+    </article>
+    <figure class="column right">
+      <img src="@MtCook.JPG" width="100%" alt="mountCook" />
+    </figure>
+  </section>
 </main>
 
 <Footer />
@@ -59,6 +64,7 @@
     background-color: #20cca4;
     border: 1px solid #19a182;
     padding: 5vh;
+    font-size: 1.3vw;
   }
   h2 {
     font-size: 130%;
@@ -74,7 +80,7 @@
     margin-right: auto;
   }
   /* come back to columns */
-  .row {
+  .columns {
     display: flex;
     justify-content: space-between;
     align-items: center;
