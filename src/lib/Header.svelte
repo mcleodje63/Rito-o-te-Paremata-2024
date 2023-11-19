8de3a3ec5ffca62dde3b20094ec3676177c2c25d
mcleodje63 <mcleodje63@craighead.school.nz>
2023-11-19 16:16:10 +1300

    fixed some formatting + textual errors

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,7 +1,7 @@
 <header>
   <h1 class="title">Rito o te Pāremata 2024</h1>
   <h2>
-    <a href="mailto:mcleodje63@craighead.school.nz"><img src="gmail.png" alt="gmail" /></a><a href="https://www.linkedin.com/in/jessie-mcleod-a44b3a249"><img src="linkedin.png" alt="linkedin" /></a>
+    <a href="mailto:jessiejammcleod@gmail.com"><img src="gmail.png" alt="gmail" /></a><a href="https://www.linkedin.com/in/jessie-mcleod-a44b3a249"><img src="linkedin.png" alt="linkedin" /></a>
     <a href="https://instagram.com/jessiejammcleod?igshid=MzMyNGUyNmU2YQ=="><img src="instagram.png" alt="instagram" /></a>
   </h2>
 </header>
@@ -20,7 +20,7 @@
     text-shadow: 0.5px 0.5px 0.5px black;
   }
   img {
-    width: 40px;
-    margin: 5px;
+    width: 1.8vw;
+    margin: 0.3vw;
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,16 +24,20 @@
   </div>
   <section class="content section columns">
     <article class="column left">
-      <p>My name is Jessie McLeod, I’m 16, and a year 12 student at Craighead Dioscean. I live in Timaru, a port town two hours south of Christchurch, and have for my whole life. This constant stability in a chaotic world has allowed me to get involved and make connections in a variety of areas which I hold a passion for. In my spare time I enjoy spending time outdoors with my friends and family, cooking, reading and writing poetry, but also coding, photography, watching tv, football and playing guitar.</p>
+      <p>My name is Jessie McLeod, I’m 16, and a year 12 student at Craighead Dioscean school. I live in Timaru, a port town two hours south of Christchurch, and have for my whole life. This constant stability in a chaotic world has allowed me to get involved and make connections in a variety of areas which I hold a passion for. In my spare time I enjoy being outdoors with my friends and family, cooking, reading and writing poetry, but also coding, photography, watching tv, football and playing guitar.</p>
     </article>
     <figure class="column right">
-      <img src="seaweek.jpg" alt="Seaweek" />
+      <a href="https://www.stuff.co.nz/timaru-herald/news/128041246/seaweek-events-raise-awareness-and-funds-in-south-canterbury"> <img src="seaweek.jpg" alt="Seaweek" /></a>
+
+      <figcaption>Beach cleanup with sustainable coastlines</figcaption>
       <!-- remember to link to news article  -->
     </figure>
   </section>
   <section class="content section columns">
     <figure class="column left">
-      <img src="scumna.JPG" alt="sumnerYouthGroup" />
+      <a href="https://www.sumred.org.nz/"> <img src="scumna.JPG" alt="sumnerYouthGroup" /></a>
+
+      <figcaption>Easter Camp with Sumner Redcliffs + St. Johns</figcaption>
     </figure>
     <article class="column right">
       <ul>
@@ -41,18 +45,19 @@
         <li>Member of national Anglican General Snyod 2024</li>
         <li>Experience working with people, from babies to seniors</li>
       </ul>
-      <p>Since the start of this year, I’ve been on Vestry at St John’s Anglican Church, and attended Christchurch Dioscean Synod, which has been a great experience for me, and I plan to remain in this role until I’ve graduated high school. Another aspect of St. Johns Church which I am heavily involved in, is our church's A Rocha’s EcoChurch group, which involves working in organising events, teaming up with people from different generations/backgrounds, and sending a lot of emails.</p>
+      <p>Since the start of this year, I’ve been on Vestry at St John’s Anglican Church, and attended Christchurch Dioscean Synod, which has been a great experience for me, and I plan to remain in this role until I’ve graduated high school. Another aspect of St. Johns Church which I am heavily involved in, is our church's A Rocha’s EcoChurch group, which involves working on organising events, teaming up with people from different generations and backgrounds, and sending a lot of emails.</p>
       <br />
     </article>
   </section>
   <section class="content section columns">
     <article class="column left">
-      <p>During my time at Craighead, I was lucky enough to be selected to attend two model EU’s held at the University of Canterbury, which allowed me to explore diplomacy and working with others. Volunteering for my community has also been very important to me, I’ve been involved in helping with the Craighead uniform shop, Sunday school, Sustainable Coastlines, early vote counting, as well as many others, and gathered over 100 hours on the SVA website.</p>
+      <p>During my time at Craighead, I was lucky enough to be selected to attend two model EU’s held at the University of Canterbury, which allowed me to explore diplomacy and working with others. Volunteering for my community has also been very important to me, I’ve been involved in helping with Craighead uniform shop, Sunday school, Sustainable Coastlines, early vote counting, as well as many others, and gathered over 100 hours on the SVA website.</p>
       <br />
-      <p>I want to be a part of Rito to discover hands on the inner workings of a system which affects me and learn more about Aotearoa New Zealand’s government. I may not know much about Parliament, however I’m naturally a curious and interested person, so this role is right for me.</p>
+      <p>I would like to be a part of Rito to discover hands on, the inner workings of a system which affects me and learn more about Aotearoa New Zealand’s government. I may not know much about Parliament, however I’m naturally a curious and interested person, so this role is right for me.</p>
     </article>
     <figure class="column right">
       <img src="@MtCook.JPG" width="100%" alt="mountCook" />
+      <figcaption>Hooker Valley walk</figcaption>
     </figure>
   </section>
 </main>
@@ -63,7 +68,7 @@
   ul {
     background-color: #20cca4;
     border: 1px solid #19a182;
-    padding: 5vh;
+    padding: 1.5vw;
     font-size: 1.3vw;
   }
   h2 {
@@ -100,8 +105,8 @@
   }
 
   .cropped-image video {
-    width: 100%; /* Make the image fill the container */
-    height: 100%; /* Make the image fill the container */
+    width: 100vw; /* Make the image fill the container */
+    height: 24.5vw; /* Make the image fill the container */
     object-fit: cover; /* Maintain aspect ratio and cover the container */
     object-position: bottom;
   }
